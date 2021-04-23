mirror/r

commit b718f8cb38e18a3de28cdcebfbeefe624c7808e9

numToBits() tweaks notably for big endian platforms
git-svn-id: https://svn.r-project.org/R/trunk@80204 00db46b3-68df-0310-9c12-caf00c1e9a41

src/main/raw.c
@@ -1,6 +1,6 @@
 /*
  *  R : A Computer Language for Statistical Data Analysis
- *  Copyright (C) 2001--2020 The R Core Team
+ *  Copyright (C) 2001--2021 The R Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Pulic License as published by
@@ -137,6 +137,23 @@ SEXP attribute_hidden do_intToBits(SEXP call, SEXP op, SEXP args, SEXP env)
     UNPROTECT(2);
     return ans;
 }
+/* copied from arithmetic.c - how reliable are compile-time endianess tests? */
+#ifdef _AIX
+#define CONST
+#elif defined(sparc) && defined (__GNUC__) && __GNUC__ == 3
+#define CONST
+#else
+#define CONST const
+#endif
+
+#ifdef WORDS_BIGENDIAN
+static CONST int hw = 0;
+static CONST int lw = 1;
+#else  /* !WORDS_BIGENDIAN */
+static CONST int hw = 1;
+static CONST int lw = 0;
+#endif /* WORDS_BIGENDIAN */
+static CONST int wo[2] = {lw, hw};
 
 // split "real" (double = 64-bit) into two 32-bit parts (which the user can split to bits):
 SEXP attribute_hidden do_numToInts(SEXP call, SEXP op, SEXP args, SEXP env)
@@ -149,9 +166,14 @@ SEXP attribute_hidden do_numToInts(SEXP call, SEXP op, SEXP args, SEXP env)
     R_xlen_t i, j = 0;
     double *x_ = REAL(x);
     for (i = 0; i < XLENGTH(x); i++) {
-	int *tmp = (int*) &(x_[i]);
-	INTEGER(ans)[j++] = tmp[0];
-	INTEGER(ans)[j++] = tmp[1];
+	// Assume sizeof(double) == 2 * sizeof(int) and int has no trap rep.
+	union {
+	    double d;
+	    int i[2];
+	} tmp;
+	tmp.d = x_[i];
+	INTEGER(ans)[j++] = tmp.i[lw];
+	INTEGER(ans)[j++] = tmp.i[hw];
     }
     UNPROTECT(2);
     return ans;
@@ -167,7 +189,13 @@ SEXP attribute_hidden do_numToBits(SEXP call, SEXP op, SEXP args, SEXP env)
     R_xlen_t i, j = 0;
     double *x_ = REAL(x);
     for (i = 0; i < XLENGTH(x); i++) {
-	uint64_t *x_i = (uint64_t*) &(x_[i]), tmp = *x_i;
+	// Assume double and uint64_t are both 64 bits.
+	union {
+	    double d;
+	    uint64_t ui64;
+	} u;
+	u.d = x_[i];
+	uint64_t tmp = u.ui64;
 	for (int k = 0; k < 64; k++, tmp >>= 1)
 	    RAW(ans)[j++] = tmp & 0x1;
     }
@@ -226,25 +254,28 @@ SEXP attribute_hidden do_packBits(SEXP call, SEXP op, SEXP args, SEXP env)
 	    }
 	    INTEGER(ans)[i] = (int) itmp;
 	} else { // 'useDouble'
-	    union {
+	    // Assume sizeof(double) == 2 * sizeof(unsigned int) and
+	    // unsigned int has no trap rep.
+	    union
+	    {
 		double d;
-		int i[2];
+		unsigned int ui[2];
 	    } u;
 	    for(int k = 0 ; k < 2 ; k++) {
-		int w = 0;
+		unsigned int w = 0;
 		for(int b = 0 ; b < 32 ; b++) {
-		    int bit /* -Wall */ = 0;
+		    unsigned int bit /* -Wall */ = 0;
 		    if (isRaw(x))
 			bit = RAW(x)[64*i + 32*k + b] & 0x1;
 		    else if (isLogical(x) || isInteger(x)) {
 			int j = INTEGER(x)[64*i + 32*k + b];
 			if (j == NA_INTEGER)
 			    error(_("argument 'x' must not contain NAs"));
-			bit = j & 0x1;
+			bit = (unsigned int) (j & 0x1);
 		    }
 		    w = w | (bit << b);
 		}
-		u.i[k] = w;
+		u.ui[wo[k]] = w; /* wo for endianess */
 	    }
 	    REAL(ans)[i] = u.d;
 	}

src/library/base/man/rawConversion.Rd
@@ -1,6 +1,6 @@
 % File src/library/base/man/rawConversion.Rd
 % Part of the R package, https://www.R-project.org
-% Copyright 2004-2020 R Core Team
+% Copyright 2004-2021 R Core Team
 % Distributed under GPL 2 or later
 
 \name{rawConversion}
@@ -67,16 +67,18 @@ numToBits(x)
   \code{numToInts()} and
   \code{numToBits()} split \code{\link{double}} precision numeric vectors
   either into to two \code{\link{integer}}s each or into 64 bits each,
-  stored as \code{raw}.
+  stored as \code{raw}.  In both cases the unpacking is least-significant
+  element first.
 }
 \details{
   \code{packBits} accepts raw, integer or logical inputs, the last two
   without any NAs.
-#ifdef unix
+
+  \code{numToBits(.)} and \code{packBits(., type="double")} are
+  \emph{inverse} functions of each other, see also the examples.
 
   Note that \sQuote{bytes} are not necessarily the same as characters,
   e.g.\sspace{}in UTF-8 locales.
-#endif
 }
 \examples{
 x <- "A test string"
@@ -85,11 +87,11 @@ is.vector(y) # TRUE
 
 rawToChar(y)
 rawToChar(y, multiple = TRUE)
-(xx <- c(y,  charToRaw("&"), charToRaw("more")))
+(xx <- c(y,  charToRaw("&"), charToRaw(" more")))
 rawToChar(xx)
 
 rawShift(y, 1)
-rawShift(y, -2)
+rawShift(y,-2)
 
 rawToBits(y)
 
@@ -119,26 +121,21 @@ data.frame(B = matrix(pBi, nrow=6, byrow=TRUE),
 ## Look at internal bit representation of ...
 
 ## ... of integers :
-##
-.Platform $ endian # "little" (typically on all platforms R runs (?))
-R <- if(.Platform $ endian == "little") rev else identity
 bitI <- function(x) vapply(as.integer(x), function(x) {
-            b <- substr(as.character(R(intToBits(x))), 2L, 2L)
+            b <- substr(as.character(rev(intToBits(x))), 2L, 2L)
             paste0(c(b[1L], " ", b[2:32]), collapse = "")
           }, "")
 print(bitI(-8:8), width = 35, quote = FALSE)
 
 ## ... of double precision numbers in format  'sign exp | mantissa'
 ## where  1 bit sign  1 <==> "-";
-##       11 bit exp   is the base-2 exponent in 2-complement form
+##       11 bit exp   is the base-2 exponent biased by 2^10 - 1 (1023)
 ##       52 bit mantissa is without the implicit leading '1'
 #
-R <- if(.Platform $ endian == "little") rev else identity
-
 ## Bit representation  [ sign | exponent | mantissa ] of double prec numbers :
 
 bitC <- function(x) noquote(vapply(as.double(x), function(x) { # split one double
-    b <- substr(as.character(R(numToBits(x))), 2L, 2L)
+    b <- substr(as.character(rev(numToBits(x))), 2L, 2L)
     paste0(c(b[1L], " ", b[2:12], " | ", b[13:64]), collapse = "")
   }, ""))
 bitC(17)
@@ -155,6 +152,10 @@ str(bx <- d2b(x)) # a  64 x 2122  raw matrix
 stopifnot( identical(bx, d2bI(x)) )
 
 ## Show that  packBits(*, "double")  is the inverse of numToBits() :
+packBits(numToBits(pi), type="double")
+bitC(2050)
+b <- numToBits(2050) 
+identical(b, numToBits(packBits(b, type="double")))
 pbx <- apply(bx, 2, packBits, type="double")
 stopifnot( identical(pbx, x))
 }

commit 06c6e60a7846b29a6059499b808c8febeacd37bd

fix breakage caused by r80204
git-svn-id: https://svn.r-project.org/R/trunk@80213 00db46b3-68df-0310-9c12-caf00c1e9a41

src/main/raw.c
@@ -137,23 +137,14 @@ SEXP attribute_hidden do_intToBits(SEXP call, SEXP op, SEXP args, SEXP env)
     UNPROTECT(2);
     return ans;
 }
-/* copied from arithmetic.c - how reliable are compile-time endianess tests? */
-#ifdef _AIX
-#define CONST
-#elif defined(sparc) && defined (__GNUC__) && __GNUC__ == 3
-#define CONST
-#else
-#define CONST const
-#endif
 
 #ifdef WORDS_BIGENDIAN
-static CONST int hw = 0;
-static CONST int lw = 1;
+#define WORDORDER_HIGH 0
+#define WORDORDER_LOW  1
 #else  /* !WORDS_BIGENDIAN */
-static CONST int hw = 1;
-static CONST int lw = 0;
+#define WORDORDER_HIGH 1
+#define WORDORDER_LOW  0
 #endif /* WORDS_BIGENDIAN */
-static CONST int wo[2] = {lw, hw};
 
 // split "real" (double = 64-bit) into two 32-bit parts (which the user can split to bits):
 SEXP attribute_hidden do_numToInts(SEXP call, SEXP op, SEXP args, SEXP env)
@@ -172,8 +163,8 @@ SEXP attribute_hidden do_numToInts(SEXP call, SEXP op, SEXP args, SEXP env)
 	    int i[2];
 	} tmp;
 	tmp.d = x_[i];
-	INTEGER(ans)[j++] = tmp.i[lw];
-	INTEGER(ans)[j++] = tmp.i[hw];
+	INTEGER(ans)[j++] = tmp.i[WORDORDER_LOW];
+	INTEGER(ans)[j++] = tmp.i[WORDORDER_HIGH];
     }
     UNPROTECT(2);
     return ans;
@@ -275,7 +266,7 @@ SEXP attribute_hidden do_packBits(SEXP call, SEXP op, SEXP args, SEXP env)
 		    }
 		    w = w | (bit << b);
 		}
-		u.ui[wo[k]] = w; /* wo for endianess */
+		u.ui[k ? WORDORDER_HIGH : WORDORDER_LOW] = w;
 	    }
 	    REAL(ans)[i] = u.d;
 	}